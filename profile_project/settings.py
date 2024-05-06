1bd5447f7d5f35d7f1b138cba535443455cb84b6
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-06 11:38:56 +0700

    - add viewset

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'profile_api'
+    'profile_api',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
