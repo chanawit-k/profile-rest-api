f9529aabbf523b58ebc6429ddcbe7fdb79192ad7
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-06 13:20:14 +0700

    - login with token

--- profile_api/views.py
+++ profile_api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import filters  # type: ignore
 from rest_framework import status, viewsets  # type: ignore
 from rest_framework.authentication import TokenAuthentication  # type: ignore
+from rest_framework.authtoken.views import ObtainAuthToken  # type: ignore
 from rest_framework.response import Response  # type: ignore
+from rest_framework.settings import api_settings  # type: ignore
 from rest_framework.views import APIView  # type: ignore
 
 from profile_api import models, permissions
@@ -113,3 +115,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
